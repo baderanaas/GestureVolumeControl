bdf877d6b7550b9d0be664242329a1f31c9098cc
baderanaas <anas.bader@insat.ucar.tn>
2023-08-29 16:14:18 +0100

    Positionning thumb and index finger and drawing line between them

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -17,6 +17,17 @@ detector = ht.HandDetector(detectionCon=0.7)
 while True:
     success, img = cap.read()
     img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    if len(lmList) != 0:
+        print(lmList[4], lmList[8])
+        
+        x1, y1 = lmList[4][1], lmList[4][2]
+        x2, y2 = lmList[8][1], lmList[8][2]
+        
+        cv2.circle(img, (x1, y1), 7, (255, 0, 255), cv2.FILLED)
+        cv2.circle(img, (x2, y2), 7, (255, 0, 255), cv2.FILLED)
+        cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
+        
     
     cTime = time.time()
     fps = 1 / (cTime - pTime)
