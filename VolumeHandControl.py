703a1994704bcd49e2acf913040c8d83d6564668
baderanaas <anas.bader@insat.ucar.tn>
2023-08-29 16:44:48 +0100

    Adding the volume changing module and make propotion with fingers' distance and making it work finally

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -3,6 +3,8 @@ import time
 import numpy as np
 import HandTracking as ht
 import math
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 
 wCam, hCam = 640, 480
 pTime = 0
@@ -14,6 +16,16 @@ cap.set(4, hCam)
 
 detector = ht.HandDetector(detectionCon=0.7)
 
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = interface.QueryInterface(IAudioEndpointVolume)
+volRange = volume.GetVolumeRange()
+minVol = volRange[0]
+maxVol = volRange[1]
+
+vol = 0
+volBar = 400
 
 while True:
     success, img = cap.read()
@@ -32,9 +44,16 @@ while True:
         cv2.line(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
         length = math.hypot(x2 - x1, y2 - y1)
-        print(length) # 168 13
-        if length<50:
+        
+        vol=np.interp(length, [13, 168], [minVol, maxVol])
+        volBar = np.interp(length, [13,168], [400, 150])
+        volume.SetMasterVolumeLevel(vol, None)
+        
+        if length < 50:
             cv2.circle(img, (cx, cy), 7, (0, 255, 0), cv2.FILLED)
+            
+    cv2.rectangle(img, (50, 150), (85,400), (0,255,0),3)
+    cv2.rectangle(img, (50, int(volBar)), (85,400), (0,255,0),cv2.FILLED)
 
     cTime = time.time()
     fps = 1 / (cTime - pTime)
